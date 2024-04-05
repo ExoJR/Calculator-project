b97869987cc5cef6e7db2ad290f74e3fe7af1e8e
Exo Jr <ravacemilian@yahoo.com>
2024-04-06 01:51:58 +0300

    I added the pressing effect for the buttons

--- calculator/src/components/Buttons.jsx
+++ calculator/src/components/Buttons.jsx
@@ -1,25 +1,63 @@
+import { useState } from "react";
+
+
 function Buttons() {
 
+  const [isPressed, setIsPressed] = useState(null)
 
-const imagesBtn = ['btn-c.png','btn-ce.png','btn-sign.png','btn-sqrt.png','btn-7.png','btn-8.png','btn-9.png','btn-prod.png','btn-4.png','btn-5.png','btn-6.png','btn-div.png','btn-1.png','btn-2.png','btn-3.png','btn-dif.png','btn-0.png','btn-dot.png','btn-sum.png','btn-equal.png',]
+  const imagesBtn = [
+    "btn-c.png",
+    "btn-ce.png",
+    "btn-sign.png",
+    "btn-sqrt.png",
+    "btn-7.png",
+    "btn-8.png",
+    "btn-9.png",
+    "btn-prod.png",
+    "btn-4.png",
+    "btn-5.png",
+    "btn-6.png",
+    "btn-div.png",
+    "btn-1.png",
+    "btn-2.png",
+    "btn-3.png",
+    "btn-dif.png",
+    "btn-0.png",
+    "btn-dot.png",
+    "btn-sum.png",
+    "btn-equal.png",
+  ];
 
+  const handleMouseDown = (index) =>{
+    setIsPressed(index)
+  };
 
-  
+  const handleMouseUp = () =>{
+    setIsPressed(null)
+  };
 
-
-return (
-  <ul className="buttons">
-    {imagesBtn.map((image, index) => (
-      <li key={index} id={`btn-${index}`} className="button" style={{
-        background: `url(/assets/${image})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '96px 46px',
-        backgroundPosition:'0px',
-      }}></li>
-    ))}
-  </ul>
-);
+  return (
+    <ul className="buttons">
+      {imagesBtn.map((image, index) => (
+        <li
+          key={index}
+          id={`btn-${index}`}
+          className="button"
+          style={{
+            background: `url(/assets/${image})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize:"108px 48px",
+            backgroundPosition:  isPressed === index ? '-54px 0px' : "0px 0px",
+          }}
+          onMouseDown={() => handleMouseDown(index)}
+          onMouseUp={handleMouseUp}
+          onMouseLeave={handleMouseUp}
+          onTouchStart={() => handleMouseDown(index)}
+          onTouchEnd={handleMouseUp}
+        ></li>
+      ))}
+    </ul>
+  );
 }
 
-
-export default Buttons
+export default Buttons;
